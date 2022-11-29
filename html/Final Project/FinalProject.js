16c8f5ec38e315bd5c7a65757ff387146aeaf87f
Lucas Azevedo <102688981+Fuguety@users.noreply.github.com>
2022-11-29 13:30:33 -0300

    Finished Project
    Files now connected
    Minor bugs and fixes
    Tweaked css

--- html/Final Project/FinalProject.js
+++ html/Final Project/FinalProject.js
@@ -8,9 +8,7 @@ const xhttp = new XMLHttpRequest();
 
 
     
-
-
-    function af(){
+    function na(){
 
         xhttp.onload = function () {
 
@@ -20,80 +18,14 @@ const xhttp = new XMLHttpRequest();
     
         }
 
-        //Africa Nairobi
-        let latitude = '-1.28';
-        let longitude = '36.82';
+        //North America Washington DC
+        let latitude = '38.8921';
+        let longitude = '-77.0241';
         xhttp.open("GET", `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current_weather=true`, true);
         xhttp.send();
-        console.log();
-
-        document.getElementById("choose_city").innerHTML = "Nairobi";
-    }
-    
-    function oc(){
-
-        xhttp.onload = function () {
-
-            requestData = JSON.parse(this.responseText);
-            console.log(requestData["current_weather"]["temperature"]);
-            document.getElementById("temperature").innerHTML = "Temperature in ºC: " + requestData["current_weather"]["temperature"];
-    
-        }
-        //Oceanic Canberra
-
-        let latitude = '-35.2820';
-        let longitude = '149.1286';
-        xhttp.open("GET", `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current_weather=true`, true);
-        xhttp.send();
-        console.log();
         
-        
-        document.getElementById("choose_city").innerHTML = "Canberra";
-
-
-    }
-
-    function as(){
-
-        xhttp.onload = function () {
-
-            requestData = JSON.parse(this.responseText);
-            console.log(requestData["current_weather"]["temperature"]);
-            document.getElementById("temperature").innerHTML = "Temperature in ºC: " + requestData["current_weather"]["temperature"];
-    
-        }
-
-        //Asia Tokyo
-
-        let latitude = '35.6785';
-        let longitude = '139.6823';
-        xhttp.open("GET", `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current_weather=true`, true);
-        xhttp.send();
-        console.log();
-
-        document.getElementById("choose_city").innerHTML = "Tokyo";
-
-    
-    }
-
-    function eu(){
-
-        xhttp.onload = function () {
-
-            requestData = JSON.parse(this.responseText);
-            console.log(requestData["current_weather"]["temperature"]);
-            document.getElementById("temperature").innerHTML = "Temperature in ºC: " + requestData["current_weather"]["temperature"];
-    
-        }
-
-        //Europe London
-        let latitude = '51.5002';
-        let longitude = '-0.1262';
-        xhttp.open("GET", `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current_weather=true`, true);
-        xhttp.send();
-        console.log();
-
-        document.getElementById("choose_city").innerHTML = "London";
+        document.getElementById("choose_city").innerHTML = "Washington DC";
+        document.getElementById("more_info").href = "./Washigton DC.html";
 
     }
     
@@ -116,31 +48,11 @@ const xhttp = new XMLHttpRequest();
         console.log();
 
         document.getElementById("choose_city").innerHTML = "Ciudad de Mexico";
+        document.getElementById("more_info").href = "./Ciudad de Mexico.html";
 
 
     }
     
-    function na(){
-
-        xhttp.onload = function () {
-
-            requestData = JSON.parse(this.responseText);
-            console.log(requestData["current_weather"]["temperature"]);
-            document.getElementById("temperature").innerHTML = "Temperature in ºC: " + requestData["current_weather"]["temperature"];
-    
-        }
-
-        //North America Washington DC
-        let latitude = '38.8921';
-        let longitude = '-77.0241';
-        xhttp.open("GET", `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current_weather=true`, true);
-        xhttp.send();
-        
-        document.getElementById("choose_city").innerHTML = "Washington DC";
-
-
-    }
-
     function sa(){
 
         xhttp.onload = function () {
@@ -159,10 +71,106 @@ const xhttp = new XMLHttpRequest();
         
         
         document.getElementById("choose_city").innerHTML = "Brasilia DF";
+        document.getElementById("more_info").href = "./Brasilia DF.html";
+
         
     }
 
+    function eu(){
+
+        xhttp.onload = function () {
+
+            requestData = JSON.parse(this.responseText);
+            console.log(requestData["current_weather"]["temperature"]);
+            document.getElementById("temperature").innerHTML = "Temperature in ºC: " + requestData["current_weather"]["temperature"];
     
+        }
+
+        //Europe London
+        let latitude = '51.5002';
+        let longitude = '-0.1262';
+        xhttp.open("GET", `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current_weather=true`, true);
+        xhttp.send();
+        console.log();
+
+        document.getElementById("choose_city").innerHTML = "London";
+        document.getElementById("more_info").href = "./London.html";
+
+
+    }
     
+    function af(){
+
+        xhttp.onload = function () {
+
+            requestData = JSON.parse(this.responseText);
+            console.log(requestData["current_weather"]["temperature"]);
+            document.getElementById("temperature").innerHTML = "Temperature in ºC: " + requestData["current_weather"]["temperature"];
+    
+        }
+
+        //Africa Nairobi
+        let latitude = '-1.28';
+        let longitude = '36.82';
+        xhttp.open("GET", `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current_weather=true`, true);
+        xhttp.send();
+        console.log();
+
+        document.getElementById("choose_city").innerHTML = "Nairobi";
+        document.getElementById("more_info").href = "./Nairobi.html";
+    }
+    
+    function as(){
+
+        xhttp.onload = function () {
+
+            requestData = JSON.parse(this.responseText);
+            console.log(requestData["current_weather"]["temperature"]);
+            document.getElementById("temperature").innerHTML = "Temperature in ºC: " + requestData["current_weather"]["temperature"];
+    
+        }
+
+        //Asia Tokyo
+
+        let latitude = '35.6785';
+        let longitude = '139.6823';
+        xhttp.open("GET", `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current_weather=true`, true);
+        xhttp.send();
+        console.log();
+
+        document.getElementById("choose_city").innerHTML = "Tokyo";
+        document.getElementById("more_info").href = "./Tokyo.html";
+
+    
+    }
+
+    function oc(){
+
+        xhttp.onload = function () {
+
+            requestData = JSON.parse(this.responseText);
+            console.log(requestData["current_weather"]["temperature"]);
+            document.getElementById("temperature").innerHTML = "Temperature in ºC: " + requestData["current_weather"]["temperature"];
+    
+        }
+        //Oceanic Canberra
+
+        let latitude = '-35.2820';
+        let longitude = '149.1286';
+        xhttp.open("GET", `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current_weather=true`, true);
+        xhttp.send();
+        console.log();
+        
+        
+        document.getElementById("choose_city").innerHTML = "Canberra";
+        document.getElementById("more_info").href = "./Canberra.html";
+
+    }
+
+    
+
+
+
+
     
     
