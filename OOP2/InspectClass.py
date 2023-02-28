cf74d5bd0bd937b322471d2c6e34a96490c14b96
Lucas Azevedo <102688981+Fuguety@users.noreply.github.com>
2023-02-27 21:58:43 -0300

    Add files via upload

---
+++ OOP2/InspectClass.py
@@ -0,0 +1,13 @@
+class Engine:
+    horsepower = 10
+
+class Vehicle(Engine):
+    vehicleEngine = Engine.horsepower
+
+
+
+
+#just neet to change wich Class you want to inspect
+print(dir(Vehicle))
+
+
