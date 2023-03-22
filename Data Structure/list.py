c7eeced9d3b15a525d48fdeb3944db5ddc504320
Lucas Azevedo <102688981+Fuguety@users.noreply.github.com>
2023-03-22 10:42:29 -0300

    Update list.py

--- Data Structure/list.py
+++ Data Structure/list.py
@@ -1,6 +1,7 @@
 class List:
     arr = []
 
+
 def insert(v):
     ls = List()
     ls.arr.insert(0, v)
@@ -14,26 +15,22 @@ def printList():
 
 def search(v):
     ls = List()
-    if v < 0:
-        print("please insert a value higher or equal to 0")
-
-    else:
-        try:
-            print(ls.arr[v - 1])
-        except:
-            print("Element not found")
+    try:
+        x = ls.arr[v]
+        print(x)
+    except:
+        print("List is empty")
 
 
 def checkArray():
     ls = List()
     try:
         if ls.arr[0] == "":
-            return False
-        else:
             return True
+        else:
+            return False
     except:
-        return False
-
+        return True
 
 def arrLenght():
     ls = List()
@@ -45,7 +42,7 @@ def lastElement():
     ls = List()
     check = checkArray()
     try:
-        if (check == False):
+        if (check != False):
             print("The list is empty")
 
         else:
@@ -56,13 +53,44 @@ def lastElement():
         print("Error")
 
 
+def remove(v):
+    ls = List()
+    x = False
+    for i in ls.arr:
+        if(i == v):
+            ls.arr.remove(v)
+            print(f"Element {v} removed")
+            x = True
+            break
+        else:
+            continue
+    
+    if(x != True):
+        print("Element not found")
+        
+def insertEnd(element):
+    ls = List()
+    check = checkArray()
+    if (check != True):
+        ls.arr.append(element)
+    
+    else:
+        print("List is empty, just adding element as first element")
+        ls.arr.insert(0, element)
+    
+def freeList():
+    ls = List()
+    ls.arr.clear()
+    print("List is now empty")
+
+
 # main
 ls = List()
 print(" Welcome to List manipulation program \n Please inster a number to choose wich action you want to do")
 
 while (True):
     try:
-        print("\n 1 - add element to list \n 2 - search element on list \n 3 - check if list is empty \n 4 - get the size of the list \n 5 - get the last element \n Enter - Exit program")
+        print(" 1 - add element to list \n 2 - search element on list \n 3 - check if list is empty \n 4 - get the size of the list \n 5 - get the last element \n 6 - Remove a specific element \n 7 - Insert a element to end of the list \n 9 - Clean list\n Enter - Exit program")
 
         print(f"\nCurrent list: {ls.arr}")
 
@@ -80,18 +108,29 @@ while (True):
             print("Wich element you want to see? ")
             element = input()
 
-            search(int(element))
+            search(element)
 
         elif (x == "3"):
-            check = checkArray()
-            print(check)
+            print(checkArray())
 
         elif (x == "4"):
-            getSize = arrLenght()
-            print(f"Size of the List: {getSize}")
+            print(arrLenght())
 
         elif (x == "5"):
             lastElement()
+        
+        elif(x == "6"):
+            print("Please type the element you want to remove:")
+            v = input()
+            remove(v)
+        
+        elif(x == "7"):
+            print("Insert element you want to add to the end of the list")
+            element = input()
+            insertEnd(element)
+            
+        elif(x == "9"):
+            freeList()
 
         elif (x == ""):
             break
@@ -102,3 +141,4 @@ while (True):
     except:
         print("Error")
         break
+
